f8f2af67c856825ac105ab28f0e0f2fc884b7c4b
Kratospidey <devtools@kratospidey.com>
2024-01-09 05:16:34 +0530

    finish fundamentals section

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const hero = document.querySelector(".hero");
-	const heroOffsetTop = hero.offsetTop;
+	const headerHeight = 50;
 
 	window.addEventListener("scroll", function () {
-		if (window.pageYOffset > heroOffsetTop) {
+		const scrollPosition = window.scrollY;
+
+		// Instead of using hero.offsetTop, use a fixed value corresponding to the desired trigger point
+		if (scrollPosition > headerHeight) {
 			hero.classList.add("blur");
 		} else {
 			hero.classList.remove("blur");
